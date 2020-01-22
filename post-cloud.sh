87595415d4bb9bfd4bc3c5cf4e90f50e8d33fb32
Boris SCHAPIRA <borisschapira@gmail.com>
2020-01-22 09:50:28 +0100

    Change base Dropbox folder

--- post-cloud.sh
+++ post-cloud.sh
@@ -6,12 +6,13 @@
 echo "Restauration des préférences"
 # Sélection du service de cloud (à décommenter si vous n'utilisez pas Dropbox, c'est le service par défaut) : https://github.com/lra/mackup/blob/master/doc/README.md
 # echo -e "[storage]\nengine = google_drive" >> ~/.mackup.cfg
+echo "[storage]\ndirectory = Bibou/Mackup" > ~/.mackup.cfg
 
 # Récupération de la sauvegarde sans demander à chaque fois l'autorisation
 mackup restore -n
 
 # Enregistrement des copies d'écran sur Dropbox
-defaults write com.apple.screencapture location -string "$HOME/Dropbox/Screenshots/"
+defaults write com.apple.screencapture location -string "$HOME/Dropbox/Bibou/Screenshots/"
 # Attention, nécessite un redémarrage ou un `killall SystemUIServer` pour être pris en compte
 
 echo "Installation de oh-my-zsh"
