1923c73b758bb896c3a9f861e2721f60f5176319
Boris SCHAPIRA <boris@schapira.dev>
2020-08-17 10:45:19 +0200

    git config

--- post-cloud.sh
+++ post-cloud.sh
@@ -19,6 +19,9 @@ echo "Installation de oh-my-zsh"
 # Installation de oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+echo "Configuration de git"
+git config --global init.defaultBranch main
+
 echo ""
 echo "ET VOILÀ !"
 echo "Il est maintenant possible d'activer d'autres dossiers dans la synchronisation Dropbox."
