f5032982b4f739f2d8f140dabe4a064f9fa70f42
Boris Schapira <boris@schapira.dev>
2024-06-25 16:08:13 +0200

    Update post-cloud.sh

--- post-cloud.sh
+++ post-cloud.sh
@@ -3,14 +3,6 @@
 ## README
 # /!\ Ce script d'installation est conçu pour mon usage. Ne le lancez pas sans vérifier chaque commande ! /!\
 
-echo "Restauration des préférences"
-# Sélection du service de cloud (à décommenter si vous n'utilisez pas Dropbox, c'est le service par défaut) : https://github.com/lra/mackup/blob/master/doc/README.md
-# echo -e "[storage]\nengine = google_drive" >> ~/.mackup.cfg
-echo "[storage]\ndirectory = Bibou/Mackup" > ~/.mackup.cfg
-
-# Récupération de la sauvegarde sans demander à chaque fois l'autorisation
-mackup restore -n
-
 # Enregistrement des copies d'écran sur Dropbox
 defaults write com.apple.screencapture location -string "$HOME/Dropbox/Bibou/Screenshots/"
 # Attention, nécessite un redémarrage ou un `killall SystemUIServer` pour être pris en compte
